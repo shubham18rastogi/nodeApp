cf4cc0776e5b612fb3d53da3adfe4210e27ad63a
Shubham <shubham.rastogi@visionael.com>
2020-06-21 23:15:56 +0530

    Fine tuning on Basic node concepts

--- node/first.js
+++ node/first.js
@@ -7,46 +7,48 @@ const event = new EventEmitter();
 const Logger = require('./logger');
 const logger = new Logger();
 
-/*function greet(person){
+function greet(person){
     console.log('Hello '+person);
 }
 
-greet('Shubham');*/
+//greet('Shubham');
 //console.log(module);
 
 //console.log(log);
-//log.print('Hello there');
-
+//log('Hello there');
+//console.log(__filename);
 //var currPath = path.parse(__filename);
 //console.log(currPath);
 
-//var totalMem = os.userInfo();
-//var freeMem = os.freemem();
-//console.log(os.platform()+' '+os.release()+' ');
-//console.log(totalMem);
-//console.log(freeMem);
+/*var totalMem = os.userInfo();
+var freeMem = os.freemem();
+console.log(os.platform()+' '+os.release()+' ');
+console.log(totalMem);
+console.log(freeMem);
+*/
+/*
+console.log('Before');
+var files = fs.readdirSync('./');
+console.log(files);
+console.log('After');
+*/
+/*console.log('Before');
+fs.readdir('./', function(err, files){
+    console.log(err);
+    //console.log(files);
+});
+console.log('After');*/
 
-// console.log('Before');
-// var files = fs.readdirSync('./');
-// console.log(files);
-// console.log('After');
+event.on('newEvent', function(){
+    console.log('Event Triggered');
+})
 
-// console.log('Before');
-// fs.readdir('./', function(err, files){
-//     console.log(files);
-// })
-// console.log('After');
+event.on('eventWithArgs', function(arg){
+    console.log(arg);
+})
 
-// event.on('newEvent', function(){
-//     console.log('Event Triggered');
-// })
-
-// event.on('eventWithArgs', function(arg){
-//     console.log(arg);
-// })
-
-// event.emit('newEvent');
-// event.emit('eventWithArgs', {arg: 'SomeValue', arg1 : 'Something else'});
+//event.emit('newEvent');
+//event.emit('eventWithArgs', {arg: 'SomeValue', arg1 : 'Something else'});
 
 logger.on('loggerEvent', function(){
     console.log('Logger Event executed');
--- node/logger.js
+++ node/logger.js
@@ -1,9 +1,10 @@
 const EventEmitter = require('events');
 const event = new EventEmitter();
 
-// var log = function(message){
-//     console.log('Log : '+message);
-// }
+/*var log = function(message){
+    console.log('Log : '+message);
+}*/
+
 
 class Logger extends EventEmitter{
     log(message){
@@ -12,5 +13,5 @@ class Logger extends EventEmitter{
     }
 }
 
-//module.exports.print = log;
+//module.exports = log;
 module.exports = Logger; 
